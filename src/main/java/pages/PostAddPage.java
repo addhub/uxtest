5894209bc184530196a2f20f07475c86da19193e
Jacqueline Pang <jp2338@cornell.edu>
2015-10-01 21:46:34 -0400

    add PostaddPage

--- src/main/java/pages/PostAddPage.java
+++ src/main/java/pages/PostAddPage.java
@@ -18,13 +18,27 @@ public class PostAddPage extends PageObject {
     @FindBy(xpath ="" )
     WebElement description;
 
+    @FindBy(xpath ="" )
+    WebElement keywords;
+
+    @FindBy(xpath ="" )
+    WebElement address;
+    @FindBy(xpath ="" )
+    WebElement state;
+    @FindBy(xpath ="" )
+    WebElement country;
+    @FindBy(xpath ="" )
+    WebElement postCode;
+
+    @FindBy(xpath ="" )
+    WebElement price;
+
     @FindBy(xpath = "")
     WebElement postButton;
 
 
-
-    public void postInfo() {
-        postButton.click();
+    public void setTitle(String title) {
+        title_textbox.sendKeys(title);
     }
 
     public void selectsCategory(String category) {
@@ -32,7 +46,32 @@ public class PostAddPage extends PageObject {
                 By.linkText(category)).click();
     }
 
-    public void setTitle(String title) {
-        title_textbox.sendKeys(title);
+    public void setdescription(String desc) {
+        title_textbox.sendKeys(desc);
     }
+
+    public void setKeywords(String key) {
+        title_textbox.sendKeys(key);
+    }
+    public void setAddress(String addr) {
+        title_textbox.sendKeys(addr);
+    }
+    public void setState(String stateInfo) {
+        title_textbox.sendKeys(stateInfo);
+    }
+    public void setCountry(String countryInfo) {
+        title_textbox.sendKeys(countryInfo);
+    }
+    public void setPostal(String postCode) {
+        title_textbox.sendKeys(postCode);
+    }
+    public void setPrice(String price) {
+        title_textbox.sendKeys(price);
+    }
+
+    public void postInfo() {
+        postButton.click();
+    }
+
+
 }
